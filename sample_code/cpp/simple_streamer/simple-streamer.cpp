918eaa2713d837ef5db39d641944958c0adb188c
Adam Kohut <adam.kohut@preact-tech.com>
2023-11-28 14:58:34 -0800

    cpp simple streamer is code complete

--- sample_code/cpp/simple_streamer/simple-streamer.cpp
+++ sample_code/cpp/simple_streamer/simple-streamer.cpp
@@ -32,12 +32,7 @@ static bool captureDistance{false};
 static std::string devicePort{DEFAULT_PORT_NAME};
 static volatile bool exitRequested{false};
 static uint16_t protocolVersion{DEFAULT_PROTOCOL_VERSION};
-static size_t verbosity{0};
 
-static std::atomic<uint32_t> amplitudeCount;
-static std::atomic<uint32_t> dcsCount;
-static std::atomic<uint32_t> distanceCount;
-static std::atomic<uint32_t> grayscaleCount;
 tofcore::CartesianTransform cartesianTransform_;
 open3d::visualization::Visualizer vis;
 
@@ -49,10 +44,6 @@ static void measurement_callback(std::shared_ptr<tofcore::Measurement_T> pData)
     cv::Mat dist_frame_resized;
     cv::Mat amp_frame;
     cv::Mat amp_frame_resized;
-
-    double *x_out;
-    double *y_out;
-    double *z_out;
     uint16_t *it_d;
     uint16_t *it_a;
     int count = 0;
@@ -66,16 +57,9 @@ static void measurement_callback(std::shared_ptr<tofcore::Measurement_T> pData)
     open3d::geometry::PointCloud point_cloud;
     std::shared_ptr<open3d::geometry::PointCloud> cloud_ptr;
     using DataType = tofcore::Measurement_T::DataType;
-    switch (pData->type())
+
+    if (pData->type() == DataType::DISTANCE_AMPLITUDE)
     {
-    case DataType::DISTANCE_AMPLITUDE:
-        ++amplitudeCount;
-        ++distanceCount;
-        if (verbosity > 0)
-        {
-            std::cout << "received DISTANCE-AMPLITUDE measurement data, packet size "
-                      << (pData->pixel_buffer().size()) << std::endl;
-        }
         // Do filtering on this opencv Mat
         dist_frame = cv::Mat(pData->height(), pData->width(), CV_16UC1, (void *)pData->distance().begin());
         cv::resize(dist_frame, dist_frame_resized, cv::Size(dist_frame.cols * 4, dist_frame.rows * 4));
@@ -113,9 +97,9 @@ static void measurement_callback(std::shared_ptr<tofcore::Measurement_T> pData)
 
         vis.ClearGeometries();
         vis.AddGeometry(cloud_ptr);
-        vis.GetViewControl().Rotate(523.6*3,523.6*3,0,0);
+        vis.GetViewControl().Rotate(523.6 * 3, 523.6 * 3, 0, 0);
         vis.GetViewControl().SetZoom(0.35);
-        vis.GetRenderOption().point_color_option_=open3d::visualization::RenderOption::PointColorOption::YCoordinate;
+        vis.GetRenderOption().point_color_option_ = open3d::visualization::RenderOption::PointColorOption::YCoordinate;
         vis.PollEvents();
         vis.UpdateRender();
         vis.UpdateGeometry();
@@ -123,87 +107,15 @@ static void measurement_callback(std::shared_ptr<tofcore::Measurement_T> pData)
         dist_frame_resized.release();
         amp_frame.release();
         amp_frame_resized.release();
-        break;
-    case DataType::DCS:
-        ++dcsCount;
-        if (verbosity > 0)
-        {
-            std::cout << "received DCS measurement data, packet size " << pData->pixel_buffer().size() << std::endl;
-        }
-        break;
-    case DataType::GRAYSCALE:
-        ++grayscaleCount;
-        if (verbosity > 0)
-        {
-            std::cout << "received GRAYSCALE measurement data, packet size " << pData->pixel_buffer().size() << std::endl;
-        }
-        break;
-    case DataType::DISTANCE:
-        ++distanceCount;
-        if (verbosity > 0)
-        {
-            std::cout << "received DISTANCE measurement data, packet size " << pData->pixel_buffer().size() << std::endl;
-        }
-        break;
-    case DataType::AMPLITUDE:
-        ++amplitudeCount;
-        if (verbosity > 0)
-        {
-            std::cout << "received AMPLITUDE measurement data, packet size "
-                      << (pData->pixel_buffer().size()) << std::endl;
-        }
-        break;
-    case DataType::AMBIENT:
-        ++amplitudeCount;
-        if (verbosity > 0)
-        {
-            std::cout << "received AMBIENT measurement data, packet size "
-                      << (pData->pixel_buffer().size()) << std::endl;
-        }
-        break;
 
-    default:
-        std::cout << "UNRECOGNIZED data type: " << static_cast<int16_t>(pData->type()) << std::endl;
     }
-    
 }
-
 namespace po = boost::program_options;
-
-class CountValue : public po::typed_value<std::size_t>
-{
-public:
-    CountValue() : CountValue(nullptr)
-    {
-    }
-
-    CountValue(std::size_t *store) : po::typed_value<std::size_t>(store)
-    {
-        // Ensure that no tokens may be passed as a value.
-        default_value(0);
-        zero_tokens();
-    }
-
-    virtual ~CountValue()
-    {
-    }
-
-    virtual void xparse(boost::any &store, const std::vector<std::string> & /*tokens*/) const
-    {
-        // Replace the stored value with the access count.
-        store = boost::any(++count_);
-    }
-
-private:
-    mutable std::size_t count_{0};
-};
 
 static void parseArgs(int argc, char *argv[])
 {
     po::options_description desc("illuminator board test");
-    desc.add_options()("help,h", "produce help message")("device-uri,p", po::value<std::string>(&devicePort)->default_value(devicePort))("protocol-version,v", po::value<uint16_t>(&protocolVersion)->default_value(DEFAULT_PROTOCOL_VERSION))("baud-rate,b", po::value<uint32_t>(&baudRate)->default_value(DEFAULT_BAUD_RATE))("amplitude,a", po::bool_switch(&captureAmxxx), "Capture DCS+Ambient or Distance Amplitude frames, (not just DCS or Distance)")("ambient", po::bool_switch(&captureAmxxx), "Capture DCS+Ambient or Distance Amplitude frames, (not just DCS or Distance)")("distance,d", po::bool_switch(&captureDistance), "Capture distance (or amplitude) frames instead of DCS frames")("verbose,V",
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           new CountValue(&verbosity),
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           "Increase verbosity of output");
+    desc.add_options()("help,h", "produce help message")("device-uri,p", po::value<std::string>(&devicePort)->default_value(devicePort))("protocol-version,v", po::value<uint16_t>(&protocolVersion)->default_value(DEFAULT_PROTOCOL_VERSION));
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -247,7 +159,7 @@ int main(int argc, char *argv[])
             std::cout << "Error retreiving lens info..." << std::endl;
             return -1;
         }
-        vis.CreateVisualizerWindow("Open3D",1480,1080,50,50,true);
+        vis.CreateVisualizerWindow("Open3D", 1480, 1080, 50, 50, true);
         vis.PollEvents();
         vis.UpdateRender();
 
@@ -259,19 +171,7 @@ int main(int argc, char *argv[])
         while (!exitRequested) // wait for ^\ or ^C
         {
             std::this_thread::sleep_until(lastTime + 1000ms);
-            lastTime = steady_clock::now();
-            const uint32_t amplitude{amplitudeCount};
-            amplitudeCount = 0;
-            const uint32_t dcs{4 * dcsCount};
-            dcsCount = 0;
-            const uint32_t distance{distanceCount};
-            distanceCount = 0;
-            const uint32_t grayscale{grayscaleCount};
-            grayscaleCount = 0;
-            std::cout << "RAW FPS: amplitude = " << amplitude << "; dcs = " << dcs
-                      << "; distance = " << distance << "; grayscale = " << grayscale
-                      << "; total = " << (amplitude + dcs + distance + grayscale) << std::endl;
-        }
+          }
         std::cout << "Shutting down..." << std::endl;
         sensor.stopStream();
     } // when scope is exited, sensor connection is cleaned up
